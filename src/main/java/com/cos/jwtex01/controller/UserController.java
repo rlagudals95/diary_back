fa482c3ad6048f04b8ac1a584bc7b3d846a27a5b
hmk1022 <hm.kim@iworkerman.com>
2022-01-17 08:43:55 +0900

    add-oauth-service

--- src/main/java/com/cos/jwtex01/controller/UserController.java
+++ src/main/java/com/cos/jwtex01/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.cos.jwtex01.controller;
 
+import java.util.Map;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.jwtex01.domain.User;
@@ -24,6 +28,14 @@ public class UserController {
 		joinReqDto.setPassword(bCryptPasswordEncoder.encode(joinReqDto.getPassword()));
 		return userRepository.save(joinReqDto.toEntity());
 	}
+	
+	@GetMapping("/Oauth/kakao")
+	public String kakao(@RequestParam Map<String, Object> param) {
+		
+		return "카카오 로그인 성공!";
+	}
+	
+	
 	
 		
 }
