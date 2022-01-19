3aa4daf653f44c0e16e103cb35509fe2856e0014
hmk1022 <hm.kim@iworkerman.com>
2022-01-20 08:48:51 +0900

    oauth...

--- src/main/java/com/cos/jwtex01/config/Constants.java
+++ src/main/java/com/cos/jwtex01/config/Constants.java
@@ -1,14 +1,21 @@
 package com.cos.jwtex01.config;
 
 public class Constants {
-	
+	//aws
 	public static final String S3_URL = "https://s3diary.s3.ap-northeast-2.amazonaws.com/";
-	
 	public static final String S3_NAME_DIARY = "s3diary";
 	public static final String S3_TYPE_CATEGORY = "02";
 	public static final String ACCESS_KEY = "AKIAYEBJVE42S46DX6XO";
 	public static final String SECRET_KEY = "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";
+	//naver
+	public static final String naverClientId = "fkIjAvzP_QnqzpRVefmI";
+	public static final String naverClientSecret = "H3gOnkdn_e";
+	//kakao
+	public static final String kakaoApiKey = "9a02524f15a3577f60c041db6dbef087";
+	public static final String kakaoAuthUrl= "https://kauth.kakao.com";
 	
+	//auth
+	public static final String redirectUri = "http://localhost:19006/";
 	
 }
 
--- src/main/java/com/cos/jwtex01/controller/UserController.java
+++ src/main/java/com/cos/jwtex01/controller/UserController.java
@@ -2,11 +2,16 @@ package com.cos.jwtex01.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.jwtex01.domain.User;
@@ -15,6 +20,7 @@ import com.cos.jwtex01.dto.JoinReqDto;
 import com.cos.jwtex01.dto.LoginReqDto;
 
 import lombok.RequiredArgsConstructor;
+import com.cos.jwtex01.config.Constants;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,16 +35,58 @@ public class UserController {
 		return userRepository.save(joinReqDto.toEntity());
 	}
 	
-	@GetMapping("/oauth2/kakao")
-	public String kakao(@RequestParam Map<String, Object> param) {
+	// 카카오 로그인창 호출
+	@RequestMapping(value = "/login/getKakaoAuthUrl")
+	public @ResponseBody String getKakaoAuthUrl(HttpServletRequest request) throws Exception {
 		
-		return "카카오 로그인 성공!";
+		System.out.println("확인!! :" + Constants.kakaoAuthUrl + "//" + Constants.kakaoApiKey + "//" + Constants.redirectUri);
+		String reqUrl = Constants.kakaoAuthUrl + "/oauth/authorize?client_id=" + Constants.kakaoApiKey + "&redirect_uri="+ Constants.redirectUri + "&response_type=code";
+		System.out.println("완성 : "+ reqUrl);
+		return reqUrl;
 	}
 	
-	
-	
-		
+	// 카카오 연동정보 조회
+//	@RequestMapping(value = "/login/oauth_kakao")
+//	public String oauthKakao(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		
+//	    String code = request.getParameter("code");
+//	    String error = request.getParameter("error");
+//	    // 카카오로그인 페이지에서 취소버튼 눌렀을경우
+//	    if (error != null) {
+//	        if (error.equals("access_denied")) {
+//	            return "redirect:/login";
+//	        }
+//	    }
+//
+//	    String accessToken = getAccessToken(code);
+//	    String kakaoUniqueNo = getKakaoUniqueNo(accessToken);
+//
+//	    if (kakaoUniqueNo != null && !kakaoUniqueNo.equals("")) {
+//	        /** 
+//	        
+//	            TO DO : 리턴받은 kakaoUniqueNo에 해당하는 회원정보 조회 후 로그인 처리 후 메인으로 이동
+//	        
+//	        */
+//
+//	    return "redirect:/";
+//	    
+//	    // 카카오톡 정보조회 실패했을경우
+//	    } else {
+//	        throw new ErrorMessage("카카오톡 정보조회에 실패했습니다.");
+//	    }
+//
+//	}
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
